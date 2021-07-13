d43c0b2788c54c39ed77a59f833c132af36f09f7
alexthecurator <edge.works.curator@gmail.com>
2021-07-13 17:16:43 +0300

    Configuring for deployment..

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     assetsDir: 'assets',
-    publicPath: process.env.NODE_ENV === 'production' ? '/index' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/keen' : '/index.html'
 }
