f5981666d1cf8a2863a6a5665603564526110576
alexthecurator <edge.works.curator@gmail.com>
2021-07-24 09:13:27 +0300

    Updating the underconstruction as per new brand

---
+++ underconstruction/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="canonical" href="https://www.keencommunications.co.tz">
+	<link rel="canonical HTTP header" href="https://www.keencommunications.co.tz">
+	<meta name="description" content="We are a Tech company based in Tanzania, with visions of innovating Africa. Our app is currently under construction.. stay tuned to find out more.">
+	<meta name="keywords" content="technology, science, tanzania tech, tech news, website, website design, innovation, dli innovation challenge, global innovation fund, innovation in entrepreneurship, technological innovation, web app, africa, the lean startup, startup company, techhub, hub, computer service, network, network service, development, developer">
+	<meta name="author" content="Alexander Cuthbert">
+	<title>Keen Comms</title>
+	<link rel="shortcut icon" type="image/jpg" href="./assets/favicon.svg"/>
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<style type="text/css" scoped>
+	
+	
+</style>
+<body>
+
+	<div class="backdrop"></div>
+
+	<section>
+		<div class="container-fluid">
+			 <div class="container d-flex justify-content-center">
+			 	<div class="content">
+			 		<div class="d-flex justify-content-center floating">
+			 			<div class="rounded-box white-bg">
+			 				<div class="d-flex justify-content-center">
+			 					<img class="kc-logo" src="./assets/keencomms.svg" alt="keen communications logo" title="keen communications">
+			 				</div>
+			 			</div>
+			 		</div>
+			 		<div class="kc-text py-4">
+			 			<div class="text-center">
+			 				<h4 class="h4 white">our <span class="yellow">web</span> app</h4>
+			 			</div>
+			 			<div class="text-center">
+			 				<h3 class="h3 white" style="font-weight: lighter;">is under construction</h3>
+			 			</div>
+			 		</div>
+
+			 		<div class="d-flex justify-content-center">
+					  <i class="loader --4"></i>
+					</div>
+
+			 		<div class="v-bottom">
+				 		<div class="text-center">
+				 			<h4 class="h4 white">See you soon...</h4>
+				 		</div>
+				 	</div>
+			 	</div>
+			 </div>
+
+			 
+		</div>
+	</section>
+
+</body>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</html>
