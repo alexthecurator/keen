e6b2a75670c0a1e55decf8ef987226c4e9d8469a
alexthecurator <edge.works.curator@gmail.com>
2021-07-30 06:18:15 +0300

    Changed approach.. to vue2 method

--- src/components/elements/getstarted.vue
+++ src/components/elements/getstarted.vue
@@ -7,7 +7,7 @@
                 id="inline-form-input-name"
                 class="mb-2 mr-sm-2 mb-sm-0"
                 placeholder="Eg:  `Ronald Jones`"
-                ref="inputName"
+                v-model="inputName"
             ></b-form-input>  
             </div>
             <div class="my-2">
@@ -16,7 +16,7 @@
                 <b-form-input 
                     id="inline-form-input-email"
                     placeholder="Email"
-                    ref="inputEmail"
+                    v-model="inputEmail"
                 ></b-form-input>
                 </b-input-group>  
             </div>
@@ -29,65 +29,88 @@
 </template>
 
 <script>
-import onMounted  from 'vue';
-import { reactive, ref } from '@vue/composition-api';
 import db from '../../firebase/configInit';
+import 'firebase/database';
 
 export default {
     name: 'getstarted',
-    setup () { 
-        const inputName = ref("");
-        const inputEmail = ref("");
-
-        const state = reactive({
-            names: "",
-            emails: ""
-        });
-
-        const subscribe = () => {
-            const subscriberRef = db.database().ref("names", "emails");
-
-            if(inputName.value !== "" || inputName.value === "" && inputEmail.value !== "" || inputEmail.value === ""){
-                return;
-            }
-
-            const subscriber = {
-                subname: state.names,
-                subemail: state.emails
-            }
-            
-            subscriberRef.push(subscriber);
-            inputName.value = "";
-            inputEmail.value = "";
-        }
-
-        onMounted(() => {
-            const subscriberRef = db.database().ref("names", "emails");
-
-            subscriberRef.on('value', snapshot => {
-            const data = snapshot.val();
-            let subscribers = [];
-
-            Object.keys(data).forEach(key => {
-            subscribers.push({
-                id: key,
-                name: data[key].subname,
-                email: data[key].subemail
-                });
-            });
-
-            state.names = inputName;
-            state.emails = inputEmail;
-
-            });
-        });
-
+    data() {
         return {
-            inputName,
-            inputEmail,
-            subscribe,
-            state
+            inputName: '',
+            inputEmail: ''
+        }
+    },
+    methods: {
+        subscribe(){
+            function writeUserData(userId, name, email) {
+                db.database().ref('users/' + userId).set({
+                    username: name,
+                    email: email,
+                });
+            }
+
+            if(this.inputName !== "" || this.inputName === "" && this.inputEmail !== "" || this.inputEmail === ""){
+                writeUserData(this.inputName,this.inputEmail);
+                console.log('success');
+            } else {
+                console.log('failed');
+            }
+
         }
     }
+    // setup () { 
+    //     const inputName = ref("");
+    //     const inputEmail = ref("");
+
+    //     const state = reactive({
+    //         names: "",
+    //         emails: ""
+    //     });
+
+    //     const subscribe = () => {
+    //         const subscriberRef = db.database().ref("names", "emails");
+
+    //         if(inputName.value !== "" || inputName.value === "" && inputEmail.value !== "" || inputEmail.value === ""){
+    //             return;
+    //         }
+
+    //         const subscriber = {
+    //             subname: state.names,
+    //             subemail: state.emails
+    //         }
+            
+    //         subscriberRef.push(subscriber);
+    //         inputName.value = "";
+    //         inputEmail.value = "";
+    //     }
+
+    //     onMounted(() => {
+    //         const subscriberRef = db.database().ref("names", "emails");
+
+    //         subscriberRef.on('value', snapshot => {
+    //         const data = snapshot.val();
+    //         let subscribers = [];
+
+    //         Object.keys(data).forEach(key => {
+    //         subscribers.push({
+    //             id: key,
+    //             name: data[key].subname,
+    //             email: data[key].subemail
+    //             });
+    //         });
+
+    //         state.names = inputName;
+    //         state.emails = inputEmail;
+
+    //         });
+    //     });
+
+    //     return {
+    //         inputName,
+    //         inputEmail,
+    //         subscribe,
+    //         state
+    //     }
+    // }
 }
 </script>
