d2ce7f1432ea1c311dc649c14079e8edda4fbee6
alexthecurator <edge.works.curator@gmail.com>
2021-08-20 12:57:12 +0300

    Updating for launch

--- underconstruction/index.php
+++ underconstruction/index.php
@@ -61,4 +61,18 @@
 </body>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+	
+	<script>
+		function countDown(){
+			var date = new Date();
+
+			var hours = date.getHours();
+			var Min = date.getMinutes();
+			var Sec = date.getSeconds();
+
+			console.log(hours + ':' + Min + ':' + Sec);
+		}
+		countDown();
+		console.log('Testing');
+	</script>
 </html>
